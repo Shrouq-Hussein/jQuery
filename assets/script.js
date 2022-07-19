6a9142fe3fa363b2a1eb8257cc8bca2ac97275c1
Shrouk Hussein <Shrouk.Hussein@itworx.com>
2022-07-19 20:12:14 +0200

    changes script.js

--- assets/script.js
+++ assets/script.js
@@ -4,13 +4,7 @@ let showSpecificContent= function(){
     $(`[data-content= ${$('.active').attr('id')}]`).show()
  }
 $("document").ready(
-    // $(".content").html($( ".active" ).text()))
-    // $(".nav-item").on("click",".nav-link",function (){
-    //     console.log($( this ).text())
-    //     $(".nav-link").removeClass("active")
-    //     $(this).addClass("active")
-    //     $(".content").html($( this ).text())
-    // }
+   
     showSpecificContent()
     
     )
